865b1e23d0d06b0ac2e15e977da6a1987c489e6c
Khoat Ba <Bakhoat2304@gmail.com>
2022-07-05 10:57:10 +0700

    update fix cors

--- src/main/java/com/example/demo_web/DemoWebApplication.java
+++ src/main/java/com/example/demo_web/DemoWebApplication.java
@@ -2,9 +2,6 @@ package com.example.demo_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "com.example.demo_web")
 public class DemoWebApplication {
@@ -13,15 +10,4 @@ public class DemoWebApplication {
 		SpringApplication.run(DemoWebApplication.class, args);
 
 	}
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/user/login").allowedOrigins("http://172.:8080");
-//			}
-//		};
-//	}
-//
-//}
 }
