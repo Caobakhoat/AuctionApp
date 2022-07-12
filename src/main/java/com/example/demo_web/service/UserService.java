073614d04ebaf20e26af5039e119f2dcb4994721
GiangLibra <75822926+giangdhptit@users.noreply.github.com>
2022-07-12 11:30:09 +0700

    update

--- src/main/java/com/example/demo_web/service/UserService.java
+++ src/main/java/com/example/demo_web/service/UserService.java
@@ -3,10 +3,13 @@ package com.example.demo_web.service;
 import com.example.demo_web.model.User;
 import com.example.demo_web.request.LoginRequest;
 import com.example.demo_web.response.BaseResponse;
+import com.example.demo_web.response.SaveUserResponse;
+import org.springframework.security.web.savedrequest.SavedCookie;
 
 public interface UserService  {
     BaseResponse checkLogin(LoginRequest req);
     User registerUser(User u);
     BaseResponse getAllUser ();
     BaseResponse checkLoginAdmin(LoginRequest req);
+    SaveUserResponse saveUser(User u);
 }
